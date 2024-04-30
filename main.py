50b822bc793731c168f0cc1e85a1eeea8feacfc2
mayur <mayurpatel78645@gmail.com>
2024-04-30 02:14:17 -0500

    add functionality using twilio to send SMS alert when it's about to rain in next 12hrs.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
 import requests
+from twilio.rest import Client
+
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = "f4b85b43229c9533232a8803096419fa"
+api_key = os.environ.get("OWM_API_KEY")
 weather_params = {
     "lat": 49.895077,
     "lon": -97.138451,
@@ -9,7 +14,6 @@ weather_params = {
     "cnt": 4,
     "units": "metric"
 }
-city_name = "winnipeg,canada"
 response = requests.get(url=OWM_ENDPOINT, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
@@ -21,4 +25,11 @@ for hour_data in weather_data["list"]:
         will_rain = True
 
 if will_rain:
-    print("Bring an umbrella")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+            body="It's going to rain today. Remember to bring an umbrella or maybe just have fun!",
+            from_='+19033548044',
+            to='+14312763707'
+        )
+    print(message.status)
